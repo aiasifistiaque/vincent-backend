2ea754dd52ae8a310134faff271e22d6f091c071
asifistiaque <asifistiaque.ai@gmail.com>
2021-06-16 16:45:00 +0600

    asdads

--- controller/order/getAllOrders.js
+++ controller/order/getAllOrders.js
@@ -7,6 +7,8 @@ const getAllOrders = asyncHandler(async (req, res) => {
 	const perPage = 10;
 	const page = req.body.page || 0;
 
+	console.log(req.body.sort);
+
 	if (option == 'Newest') sort = '-createdAt';
 	else if (option == 'Oldest') sort = 'createdAt';
 	else if (option == 'Pending') {
@@ -22,9 +24,9 @@ const getAllOrders = asyncHandler(async (req, res) => {
 				.sort(sort)
 				.skip(page * perPage)
 				.limit(perPage);
-			res.status(200).json({ orders: orders, count: count });
+			return res.status(200).json({ orders: orders, count: count });
 		} catch (e) {
-			res.status(500);
+			return res.status(500);
 			throw new Error('there was an error');
 		}
 	} else if (option == 'Completed') {
@@ -35,9 +37,94 @@ const getAllOrders = asyncHandler(async (req, res) => {
 				.sort(sort)
 				.skip(page * perPage)
 				.limit(perPage);
+			return res.status(200).json({ orders: orders, count: count });
+		} catch (e) {
+			return res.status(500);
+			throw new Error('there was an error');
+		}
+	} else if (option == 'Archived') {
+		try {
+			const count = await Order.countDocuments({ status: 'archived' });
+			const orders = await Order.find({ status: 'archived' })
+				.populate('user', 'id name')
+				.sort(sort)
+				.skip(page * perPage)
+				.limit(perPage);
+			return res.status(200).json({ orders: orders, count: count });
+		} catch (e) {
+			return res.status(500);
+			throw new Error('there was an error');
+		}
+	} else if (option == 'Cancelled') {
+		try {
+			const count = await Order.countDocuments({ status: 'cancelled' });
+			const orders = await Order.find({ status: 'cancelled' })
+				.populate('user', 'id name')
+				.sort(sort)
+				.skip(page * perPage)
+				.limit(perPage);
 			res.status(200).json({ orders: orders, count: count });
 		} catch (e) {
-			res.status(500);
+			return res.status(500);
+			throw new Error('there was an error');
+		}
+	} else if (option == 'Shipped') {
+		try {
+			const count = await Order.countDocuments({ status: 'shipped' });
+			const orders = await Order.find({ status: 'shipped' })
+				.populate('user', 'id name')
+				.sort(sort)
+				.skip(page * perPage)
+				.limit(perPage);
+			return res.status(200).json({ orders: orders, count: count });
+		} catch (e) {
+			return res.status(500);
+			throw new Error('there was an error');
+		}
+	} else if (option == 'Confirmed') {
+		try {
+			const count = await Order.countDocuments({ status: 'confirmed' });
+			const orders = await Order.find({ status: 'confirmed' })
+				.populate('user', 'id name')
+				.sort(sort)
+				.skip(page * perPage)
+				.limit(perPage);
+			return res.status(200).json({ orders: orders, count: count });
+		} catch (e) {
+			console.log(e);
+			return res.status(500);
+			throw new Error('there was an error');
+		}
+	} else if (option == 'Ready For Shipping') {
+		try {
+			const count = await Order.countDocuments({
+				status: 'ready for shipping',
+			});
+			const orders = await Order.find({ status: 'ready for shipping' })
+				.populate('user', 'id name')
+				.sort(sort)
+				.skip(page * perPage)
+				.limit(perPage);
+			return res.status(200).json({ orders: orders, count: count });
+		} catch (e) {
+			console.log(e);
+			return res.status(500);
+			throw new Error('there was an error');
+		}
+	} else if (option == 'New Orders') {
+		try {
+			const count = await Order.countDocuments({
+				status: 'Order Placed',
+			});
+			const orders = await Order.find({ status: 'Order Placed' })
+				.populate('user', 'id name')
+				.sort(sort)
+				.skip(page * perPage)
+				.limit(perPage);
+			return res.status(200).json({ orders: orders, count: count });
+		} catch (e) {
+			console.log(e);
+			return res.status(500);
 			throw new Error('there was an error');
 		}
 	}
